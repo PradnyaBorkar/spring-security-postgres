24d57e0fb79e984178a8bd4ef2e58f5c2e55f898
Pradnya Borkar <pradnya.borkar@springernature.com>
2021-10-31 22:36:02 +0530

    initial commit

---
+++ src/main/java/com/example/springsecuritypostgres/ServletInitializer.java
@@ -0,0 +1,13 @@
+package com.example.springsecuritypostgres;
+
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
+
+public class ServletInitializer extends SpringBootServletInitializer {
+
+	@Override
+	protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+		return application.sources(SpringSecurityPostgresApplication.class);
+	}
+
+}
